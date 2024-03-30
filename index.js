4a76b5806e03b97fdcd085fc44d6c9dd795568c2
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-03-31 00:14:02 +0100

    update

--- index.js
+++ index.js
@@ -7,20 +7,15 @@ let colorContainer = document.getElementById('color-container')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(seedColor.value)
-   
-
+    
     const seedColorOption = seedColor.value.slice(1)
-    console.log(seedColorOption)
     const themeOption = theme.value
-    console.log(themeOption)
     let colorHtml = ``
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColorOption}&mode=${themeOption}&count=6`)
     .then(res => res.json())
     .then(data => {
        
-      
        for (let color of data.colors) {
 
         colorHtml +=  `
@@ -31,44 +26,8 @@ form.addEventListener('submit', (e) => {
             </div>
       
         `
-        console.log(colorHtml)
         colorContainer.innerHTML = colorHtml
        
        }
-      console.log(seedColorOption)
-
     })
 })
-
-
-
-
-
-
-
-// colorHtml += `
-// <div class="grid grid-cols-5">
-//      
-//      <div class="flex flex-col justify-between h-[300px]">
-//          <div class=" bg-purple-600 h-[100%]"></div>
-//          <p class="text-center">#F55A5A</p>
-//      </div>
-//      <div class="flex flex-col justify-between h-[300px]">
-//          <div class=" bg-red-600 h-[100%]"></div>
-//          <p class="text-center">#F55A5A</p>
-//      </div>
-//      <div class="flex flex-col justify-between h-[300px]">
-//          <div class=" bg-pink-600 h-[100%]"></div>
-//          <p class="text-center">#F55A5A</p>
-//      </div>
-//      <div class="flex flex-col justify-between h-[300px]">
-//          <div class=" bg-yellow-600 h-[100%]"></div>
-//          <p class="text-center">#F55A5A</p>
-//      </div>
-//      <div class="flex flex-col justify-between h-[300px]">
-//          <div class=" bg-slate-600 h-[100%]"></div>
-//          <p class="text-center">#F55A5A</p>
-//      </div>
-//  </div>   
-   
-// `
